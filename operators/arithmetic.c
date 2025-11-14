406609bfd9c678b74eb94bee9b85bf447beb0eb1
Advait Hande <advaithande01@gmail.com>
2025-11-14 17:30:57 +0530

    Add files via upload

---
+++ operators/arithmetic.c
@@ -0,0 +1,14 @@
+// Ansh Sonkusare 25070521082
+
+#include <stdio.h>
+int main(){
+	int inp1 , inp2;
+	scanf("%d" , &inp1);
+	scanf("%d" , &inp2);
+	printf("%d\n" , inp1 + inp2);
+	printf("%d\n" , inp1 - inp2);
+	printf("%d\n" , inp1*inp2);
+	printf("%d\n" , inp1 / inp2);
+	printf("%d\n" , inp1 % inp2);
+	return 0;
+}
---
+++ operators/relational.c
@@ -0,0 +1,37 @@
+// Ansh Sonkusare 25070521082
+
+#include <stdio.h>
+
+int main(){
+	int inp1 , inp2;
+
+	printf("Enter two numbers: ");
+	scanf("%d %d" , &inp1,&inp2);
+
+	printf("%d < %d: " , inp1 , inp2);
+	printf("%s" ,inp1 < inp2 ? "True" : "False");
+	printf("\n");
+
+	printf("%d <= %d: ", inp1 , inp2);
+	printf("%s" ,inp1 <= inp2 ? "True" : "False");
+	printf("\n");
+
+	printf("%d > %d: ", inp1 , inp2);
+	printf("%s" ,inp1 > inp2 ? "True" : "False");
+	printf("\n");
+
+	printf("%d >= %d: ", inp1 , inp2);
+	printf("%s" ,inp1 >= inp2 ? "True" : "False");
+	printf("\n");
+
+	printf("%d == %d: ", inp1 , inp2);
+	printf("%s" ,inp1 == inp2 ? "True" : "False");
+	printf("\n");
+
+	printf("%d != %d: ", inp1 , inp2);
+	printf("%s" ,inp1 != inp2 ? "True" : "False");
+	printf("\n");
+
+	return 0;
+
+}
